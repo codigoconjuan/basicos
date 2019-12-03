58172926bc0f992b5bbe48faab18ee3ef887b54d
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-02 19:47:54 -0600

    introducción al state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+
+
+  // Crear listado de productos
+  const [ productos, guardarProductos ] = useState([
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Camisa VueJS', precio: 40 },
+    { id: 3, nombre: 'Camisa Node.js', precio: 30 },
+    { id: 4, nombre: 'Camisa Angular', precio: 20 },
+  ]);
 
 
   // Obtener la fecha
