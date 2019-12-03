37cf2952befe3081c40cfe5d654421f06725ff92
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-02 18:12:45 -0600

    creando el primer componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hola Mundo</h1>
+        <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+
+
+    return(
+        <h1>Desde el header</h1>
+    )
+}
+
+export default Header;
