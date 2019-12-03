52b86f2bf9a51b31cbd666499c4e2b9eb971a551
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-03 17:15:56 -0600

    boton de comprar

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -3,14 +3,24 @@ import React from 'react';
 const Producto = ({producto}) => {
 
 
-    const { nombre, precio, id } = producto
+    const { nombre, precio, id } = producto;
+
+    // Agregar producto al carrito
+    const seleccionarProducto = id => {
+        console.log('comprando... ', id);
+    }
 
     return ( 
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button 
+                type="button"
+                onClick={ () => seleccionarProducto(id) }
+            >Comprar</button>
         </div>
      );
 }
+
  
 export default Producto;
