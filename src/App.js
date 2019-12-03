c7cb3bfd5f1affb817081213df3a92fca85307c0
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-03 17:51:56 -0600

    listando productos del carrito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 
 function App() {
 
@@ -36,6 +37,10 @@ function App() {
                 agregarProducto={agregarProducto}
             />
         ))}
+
+        <Carrito 
+            carrito={carrito}
+        />
 
         <Footer 
           fecha={fecha}
---
+++ src/components/Carrito.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './carrito.css';
+import Producto from './Producto';
+
+const Carrito = ({carrito}) => ( 
+    <div className="carrito">
+        <h2>Tu carrito de compras</h2>
+
+        {carrito.length === 0 
+        ? <p>No hay elementos en el carrito</p>
+        : carrito.map(producto => (
+            <Producto 
+                key={producto.id}
+                producto={producto}
+            />
+        )) }
+    </div>
+
+);
+ 
+export default Carrito;
